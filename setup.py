fdca07e6639dd3095cd1aca2d4ea94af26b2b562
Matthew Elwin <elwin@northwestern.edu>
2022-11-14 21:56:28 -0600

    Initial package creation

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'pointcloud_demo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='elwin',
+    maintainer_email='elwin@northwestern.edu',
+    description='Demonstrate Point Cloud Processing',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'table = pointcloud_demo.tablefinder:table_entry'
+        ],
+    },
+)

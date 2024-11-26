a7d8fb16bae6b11ef035ac28e83895db72dc65db
Matthew Elwin <elwin@northwestern.edu>
2024-11-25 21:57:21 -0600

    Added voxelization

--- src/tablefinder.cpp
+++ src/tablefinder.cpp
@@ -36,6 +36,7 @@ class TableFind(Node):
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "pcl/point_types.h"
 #include "pcl/filters/crop_box.h"
+#include "pcl/filters/voxel_grid.h"
 #include "pcl_conversions/pcl_conversions.h"
 
 class TableFind : public rclcpp::Node
@@ -56,6 +57,7 @@ public:
         Node("table_find")
     {
         cropped_pub = create_publisher<sensor_msgs::msg::PointCloud2>("pcl_cropped", 10);
+        voxel_pub = create_publisher<sensor_msgs::msg::PointCloud2>("pcl_voxel", 10);
         subscriber = create_subscription<sensor_msgs::msg::PointCloud2>
             ("pcl_handler", 10, [this](const sensor_msgs::msg::PointCloud2 & pointcloud_msg)
             {
@@ -63,38 +65,44 @@ public:
                 auto pcl_in = std::make_shared<pcl::PCLPointCloud2>();
                 pcl_conversions::toPCL(pointcloud_msg, *pcl_in);
 
+                // Setup a CropBox filter to crop the point cloude
                 pcl::CropBox<pcl::PCLPointCloud2> crop_box{};
                 /// minimum corner to crop, in homogeneous coordinates
                 crop_box.setMin({-0.75, -0.6, 0.1, 1.0});
                 // maximum corner to crob p in homogeneous coordinates
                 crop_box.setMax({0.5, 0.1, 2.0, 1.0});
+
+                // Provide the input point cloud
                 crop_box.setInputCloud(pcl_in);
 
+                // apply the crop box filter to the output pointcloud
                 auto pcl_cropped = std::make_shared<pcl::PCLPointCloud2>();
-                // apply the crop box filter
                 crop_box.filter(*pcl_cropped);
 
-                // Convert output point cloud to a ros messages
+                // Convert output point cloud to a ros message and publish
                 sensor_msgs::msg::PointCloud2 cropped_msg;
                 pcl_conversions::fromPCL(*pcl_cropped, cropped_msg);
                 cropped_pub->publish(cropped_msg);
-                //  self._cropped.publish(cropped_msg)
+
+
+                // Downsample the point cloud to create a less dense point cloud which
+                // decreases processing time and puts points on a regular grid
+                // Not necessary but it does help performance
+                pcl::VoxelGrid<pcl::PCLPointCloud2> voxel_filter;
+                // parameters for the voxel filter: voxels are 0.01m x 0.01m x 0.01m
+                voxel_filter.setLeafSize(0.01, 0.01, 0.01);
+                voxel_filter.setInputCloud(pcl_cropped);
+
+                // apply the voxel filter
+                auto pcl_voxel = std::make_shared<pcl::PCLPointCloud2>();
+                voxel_filter.filter(*pcl_voxel);
+
+                // convert to a ROS 2 message and publish it
+                sensor_msgs::msg::PointCloud2 voxel_msg;
+                pcl_conversions::fromPCL(*pcl_voxel, voxel_msg);
+                voxel_pub->publish(voxel_msg);
+
             /*
-        # Downsample the point cloud to create a less dense point cloud which
-        # decreases processing time and puts points on a regular grid
-        # Not necessary but it does help performance
-        voxel_filter = pcl_cropped.make_voxel_grid_filter()
-        voxel_filter.set_leaf_size(0.01, 0.01, 0.01)
-        pcl_voxel = voxel_filter.filter()
-
-        voxel_msg = sensor_msgs_py.point_cloud2.create_cloud_xyz32(
-            pcl_msg.header,
-            pcl_voxel.to_array()
-        )
-
-        # publish the voxelized point cloud
-        self._voxel.publish(voxel_msg)
-
         # segment the table from the objects
         segmenter = pcl_voxel.make_segmenter()
         segmenter.set_model_type(pcl.SACMODEL_PLANE)
@@ -120,6 +128,7 @@ public:
     }
 private:
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr cropped_pub;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr voxel_pub;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscriber;
 
 };
